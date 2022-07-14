3d0c5af2da60b754094705a99d3d4b2baca4b8e8
Ricarddo19 <105021488+Ricarddo19@users.noreply.github.com>
2022-07-14 17:40:33 -0400

    Conexión
    YuriBD
    starUP
    ETC

---
+++ WorkersCompany/Context/YuriDBContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WorkersCompany.Models;
+
+namespace WorkersCompany.Context
+{
+    public class YuriDBContext : DbContext
+    {
+        public YuriDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<ContactosEmerg> ContactosEmergs { get; set; }
+        public DbSet<DatosLaborales> DatosLaborales { get; set; }
+        public DbSet<Trabajador> Trabajadors { get; set; }
+        public DbSet<CargaFamiliar> CargaFamiliars { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Usuario>().ToTable("Usuario");
+            modelBuilder.Entity<ContactosEmerg>().ToTable("ContactosEmerg");
+            modelBuilder.Entity<DatosLaborales>().ToTable("DatosLaborales");
+            modelBuilder.Entity<Trabajador>().ToTable("Trabajador");
+            modelBuilder.Entity<CargaFamiliar>().ToTable("CargaFamiliar");
+        }
+
+
+
+    }
+}
--- WorkersCompany/Startup.cs
+++ WorkersCompany/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -7,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WorkersCompany.Context;
 
 namespace WorkersCompany
 {
@@ -23,6 +25,9 @@ namespace WorkersCompany
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<YuriDBContext>(
+                item => item.UseSqlServer(Configuration.GetConnectionString("Conexion"))
+                );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
