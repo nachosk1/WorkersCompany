ec74d7a07b3dc74511669bb5e70fb15b8396bfca
nachosk1 <pedroale328@gmail.com>
2022-07-14 16:35:34 -0400

    change

---
+++ WorkersCompany/Models/Trabajador.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkersCompany.Models{
+    public class Trabajador{
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Rut { get; set; }
+        public string Sexo { get; set; }
+        public string Direccion { get; set; }
+        public int Telefono { get; set; }
+    }
+}
