a5a8105f445b7ae23dce1544c31b0444795589b7
nachosk1 <pedroale328@gmail.com>
2022-07-14 20:48:19 -0400

    cha

---
+++ WorkersCompany/Controllers/LoginController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WorkersCompany.Context;
+using WorkersCompany.Models;
+
+namespace WorkersCompany.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly YuriDBContext _context;
+        public LoginController(YuriDBContext context) {
+            _context = context; 
+        }
+        // GET: LoginController
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(Usuario usuario)
+        {
+            if(usuario.Username.Equals("Admin") && usuario.Password.Equals("1234")){
+                return RedirectToAction("Index", "Usuario");
+            }
+            else{
+                var user = (from u in _context.Usuarios
+                           where u.Username.Equals(usuario.Username)
+                           && u.Password.Equals(usuario.Password)
+                           select u).FirstOrDefault();
+                           
+                if (user != null){
+                    return RedirectToAction("Index", "Trabajador");
+                }
+                else{
+                    return View();
+                }
+            }
+            
+        }
+
+
+    }
+}
